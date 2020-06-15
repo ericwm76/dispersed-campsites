ed1c6e852829c0089ceca8efa695b148a96c330d
ericwm76 <eric.meldrum@gmail.com>
2020-06-15 14:39:10 -0400

    Revert "Add markers to map"
    This reverts commit a220ee67386a7af0ace6a924c519524c2608ed0d.

--- index.js
+++ index.js
@@ -3,78 +3,11 @@ const MAPBOX_STYLE = 'mapbox://styles/ericwm76/ckbgrggqo0lxb1in0fhr3f00j';
 
 mapboxgl.accessToken = ACCESS_TOKEN;
 
-const geojson = {
-  'type': 'FeatureCollection',
-  'features': [
-    {
-      'type': 'Feature',
-      'properties': {
-        'message': 'Pikes Peak!',
-        'iconSize': [60, 60]
-      },
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [38.8409, -105.0423]
-      }
-    },
-    {
-      'type': 'Feature',
-      'properties': {
-        'message': 'Mount Evans!',
-        'iconSize': [50, 50]
-      },
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [39.5883, -105.6438]
-      }
-    },
-    {
-      'type': 'Feature',
-      'properties': {
-        'message': 'Mount Princeton!',
-        'iconSize': [40, 40]
-      },
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [38.7494, -106.2425]
-      }
-    }
-  ]
-};
-
-const map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
   container: 'map',
+  
+
   style: MAPBOX_STYLE,
-  center: [-104.8214, 38.8339],
-  zoom: 9
+  center: [-77.04, 38.907],
+  zoom: 14
 });
-
-map.addControl(new mapboxgl.NavigationControl());
-
-const marker = new mapboxgl.Marker()
-.setLngLat([38.8409, -105.0423])
-.setDraggable(true)
-.addTo(map);
-
-// add markers to map
-// geojson.features.forEach(marker => {
-//   // create a DOM element for the marker
-//   var el = document.createElement('div');
-//   el.className = 'marker';
-//   el.style.backgroundImage =
-//   './tent-icon.svg' +
-//   marker.properties.iconSize.join('/') +
-//   '/)';
-//   el.style.width = marker.properties.iconSize[0] + 'px';
-//   el.style.height = marker.properties.iconSize[1] + 'px';
-   
-//   el.addEventListener('click', function() {
-//   window.alert(marker.properties.message);
-//   });
-   
-//   // add marker to map
-//   new mapboxgl.Marker(el)
-//   .setLngLat(marker.geometry.coordinates)
-//   .setDraggable(true)
-//   .addTo(map);
-// });
