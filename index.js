ccc5ee2812ffc9f21790ba48acb33bab563b1a4f
ericwm76 <eric.meldrum@gmail.com>
2020-06-15 13:13:02 -0400

    Set up HTML

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const ACCESS_TOKEN = 'pk.eyJ1IjoiZXJpY3dtNzYiLCJhIjoiY2tiZ3AxazVyMHpneDM1bnlzaGhxY2J2NSJ9.ZDHqnx4vzdlkB0m0nBEjEw';
 const MAPBOX_STYLE = 'mapbox://styles/ericwm76/ckbgpu2f739fr1inn2ozkcs59/draft';
 
-
 mapboxgl.accessToken = ACCESS_TOKEN;
 
 var map = new mapboxgl.Map({
